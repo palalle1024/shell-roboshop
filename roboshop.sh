4e2aaefdc47d8bfb706e16556faab52f5c343caa
palalle <palalle1024@gmail.com>
2025-05-23 09:24:23 -0500

    hello world

--- roboshop.sh
+++ roboshop.sh
@@ -7,9 +7,11 @@ ZONE_ID="Z00673021VD75JSZ5QC6N"
 DOMAIN_NAME="palalle.site"
 
 
-for instance in ${INSTANCES[@]}
+#for instance in ${INSTANCES[@]}
+
+for instance in $@
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-00224112cda97e2f8 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-00224112cda97e2f8 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
@@ -38,3 +40,5 @@ do
         }]
     }'
 done
+
+
