1f084ebe168f8025d226beac78c85a8d9a0055e2
palalle <palalle1024@gmail.com>
2025-05-24 04:10:04 -0500

    mongo

--- catalogue.sh
+++ catalogue.sh
@@ -43,8 +43,14 @@ VALIDATE $? "Enabling nodejs:20"
 dnf install nodejs -y &>>LOG_FILE
 VALIDATE $? "Installing nodejs:20"
 
-useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>LOG_FILE
-VALIDATE $? "Creating the roboshop system user"
+id roboshop
+if [ $? -ne 0 ]
+then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>LOG_FILE
+    VALIDATE $? "Creating roboshop system user"
+else
+    echo -e "system user roboshop already created  ... $Y SKIPPING $N"
+fi
 
 mkdir -p /app 
 VALIDATE $? "Creating app Directory"
