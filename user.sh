7994bb34e2476b9f239abc0fe56956d404227a98
palalle <palalle1024@gmail.com>
2025-05-26 04:52:19 -0500

    mongo

--- user.sh
+++ user.sh
@@ -82,7 +82,7 @@ systemctl start user
 VALIDATE $? "Starting user"
 
 END_TIME=$(date +%s)
-TOTAL_TIME=$( ( $END_TIME - $START_TIME ) )
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
 
 echo -e "Script exection completed successfully,  $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
 
