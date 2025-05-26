66cc7b83acf7a21c51be36087c73eaaea74042ed
palalle <palalle1024@gmail.com>
2025-05-26 04:55:58 -0500

    mongo

--- Redis.sh
+++ Redis.sh
@@ -34,24 +34,24 @@ VALIDATE () {
         fi 
 }
 
-dnf module disable redis -y
+dnf module disable redis -y &>>$LOG_FILE
 VALIDATE $? "Disabling Default Redis version"
 
-dnf module enable redis:7 -y
+dnf module enable redis:7 -y &>>$LOG_FILE
 VALIDATE $? "enabling Redis:7"
 
-dnf install redis -y 
+dnf install redis -y  &>>$LOG_FILE
 VALIDATE $? "installing redis"
 
-sed -i  -e 's/127.0.0.1/0.0.0.0/g'  -e '/proteccted-mode/' c 'protected-mode no' /etc/redis/redis.conf 
+sed -i  -e 's/127.0.0.1/0.0.0.0/g'  -e '/protected-mode/' c 'protected-mode no' /etc/redis/redis.conf  &>>$LOG_FILE
 VALIDATE $? "Edited redis.conf to accept remote connections"
 
-systemctl enable redis 
-systemctl start redis 
+systemctl enable redis &>>$LOG_FILE
+systemctl start redis &>>$LOG_FILE
 VALIDATE $? "starting redis"
 
 END_TIME=$(date +%s)
-TOTAL_TIME=$( ( $END_TIME - $START_TIME ) )
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
 
 echo -e "Script exection completed successfully,  $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
 
--- mysql.sh
+++ mysql.sh
@@ -52,7 +52,7 @@ mysql_secure_installation --set-root-pass $MYSQL_ROOT_PASSWORD
 VALIDATE $? "setting MYSQL root password"
 
 END_TIME=$(date +%s)
-TOTAL_TIME=$( ( $START_TIME - $END_TIME ) )
+TOTAL_TIME=$(( $START_TIME - $END_TIME ))
 
 
 echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
